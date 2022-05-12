f0784f2ab6c1c97abadffbd8a9ea1d17d0b9873c
helamh96 <94868147+helamh96@users.noreply.github.com>
2022-05-12 09:02:19 -0500

    Update notesPatterns.js

--- notesPatterns.js
+++ notesPatterns.js
@@ -1,5 +1,9 @@
 "use strict"
 
+import {model} from "./model.js"
+import {presenter} from "./presenter.js"
+import {publiSub} from "./pubSub.js"
+
 window.addEventListener("load", function () {
   function getDataModelLogger (topic, p) {
     p.data = model.getActiveNotes("notes", true)
