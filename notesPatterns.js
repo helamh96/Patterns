ac191232a2e08aea533a7bc70ee1633d60268e00
helamh96 <94868147+helamh96@users.noreply.github.com>
2022-05-31 10:14:25 -0500

    Update notesPatterns.js

--- notesPatterns.js
+++ notesPatterns.js
@@ -1,11 +1,13 @@
-"use strict"
-
-import {model} from "./model.js"
-import {presenter} from "./presenter.js"
+import models from "./model.js"
 import {publiSub} from "./pubSub.js"
+import  presenters from "./presenter.js"
+
+export const presenter = presenters(publiSub).getInstance()
+export const model = models().getInstance()
 
 window.addEventListener("load", function () {
-  function getDataModelLogger (topic, p) {
+  function getDataModelLogger() {
+    let p = arguments[1]
     p.data = model.getActiveNotes("notes", true)
   }
   publiSub.subscribe("getDataPresenter", getDataModelLogger)
