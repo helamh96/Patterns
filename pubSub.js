cd575cb0365457719b2b13c6b5d3fda488a3dd58
helamh96 <94868147+helamh96@users.noreply.github.com>
2022-05-31 10:15:49 -0500

    Update pubSub.js

--- pubSub.js
+++ pubSub.js
@@ -1,44 +1,42 @@
 export const publiSub = {};
 
-(function (myObject) {
-  const topics = {}
-  let subUid = -1
+const topics = {}
+let subUid = -1
 
-  myObject.publish = function (topic, args) {
-    if (!topics[topic]) {
-      return false
-    }
-    const subscribers = topics[topic]
-    let len = subscribers ? subscribers.length : 0
-    while (len--) {
-      subscribers[len].func(topic, args)
-    }
-    return this
+publiSub.publish = function (topic, args) {
+  if (!topics[topic]) {
+    return false
   }
-
-  myObject.subscribe = function (topic, func) {
-    if (!topics[topic]) {
-      topics[topic] = []
-    }
-    const token = (++subUid).toString()
-    topics[topic].push({
-      token: token,
-      func: func
-    })
-    return token
+  const subscribers = topics[topic]
+  let len = subscribers ? subscribers.length : 0
+  while (len--) {
+    subscribers[len].func(topic, args)
   }
+  return this
+}
 
-  myObject.unsubscribe = function (token) {
-    for (const m in topics) {
-      if (topics[m]) {
-        for (let i = 0, j = topics[m].length; i < j; i++) {
-          if (topics[m][i].token === token) {
-            topics[m].splice(i, 1)
-            return token
-          }
+publiSub.subscribe = function (topic, func) {
+  if (!topics[topic]) {
+    topics[topic] = []
+  }
+  const token = (++subUid).toString()
+  topics[topic].push({
+    token: token,
+    func: func
+  })
+  return token
+}
+
+publiSub.unsubscribe = function (token) {
+  for (const m in topics) {
+    if (topics[m]) {
+      for (let i = 0, j = topics[m].length; i < j; i++) {
+        if (topics[m][i].token === token) {
+          topics[m].splice(i, 1)
+          return token
         }
       }
     }
-    return this
   }
-}(publiSub))
+  return this
+}
