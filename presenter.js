24fd2d4794479a0bbfb2a2d529680d3c26c04582
helamh96 <94868147+helamh96@users.noreply.github.com>
2022-06-06 11:15:15 -0500

    Update presenter.js

--- presenter.js
+++ presenter.js
@@ -2,15 +2,23 @@ import views from "./view.js"
 import {publiSub} from "./pubSub.js"
 import {presenter, model} from "./notesPatterns.js"
 
-const getDP = "getDataPresenter"
-const view = views(publiSub).getInstance()
+const getDP="getDataPresenter"
+const getDatesP="getDatesPresenter"
+const saveNoteP="saveNotePresenter"
+const undoActionP="undoActionPresenter"
+const InterchageNotesP="interchageNotesP"
+const editNoteP="editNotePresenter"
+const deleteNoteP="deleteNotePresenter"
+const filterNotesP="filterNotesPresenter"
+
+const view=views(publiSub).getInstance()
 
 function presenters() {
     let presenterInstance;
     return {
       getInstance: function () {
         if (!presenterInstance) {
-          presenterInstance = init()
+          presenterInstance=init()
         }
         return presenterInstance
       }
@@ -32,22 +40,22 @@ function init () {
 }
 
 function saveNote (note) {
-  publiSub.publish("saveNotePresenter", note)
+  publiSub.publish(saveNoteP, note)
   publiSub.publish(getDP, this)
 }
 
 function editNote (note, ids) {
-  publiSub.publish("editNotePresenter", [note, ids])
+  publiSub.publish(editNoteP, [note, ids])
   publiSub.publish(getDP, this)
 }
 
 function deleteNote (ids) {
-  publiSub.publish("deleteNotePresenter", ids)
+  publiSub.publish(deleteNoteP, ids)
   publiSub.publish(getDP, this)
 }
 
 function dates(ids) {
-  publiSub.publish("getDatesPresenter", [this, ids])
+  publiSub.publish(getDatesP, [this, ids])
   return {
     creation: this.creationDate,
     modification: this.lastMod
@@ -55,17 +63,17 @@ function dates(ids) {
 }
 
 function filterNotes (filter) {
-  publiSub.publish("filterNotesPresenter", filter)
+  publiSub.publish(filterNotesP, filter)
   publiSub.publish(getDP, this)
 }
 
 function interchangeNotes (start, end) {
-  publiSub.publish("interchageNotesP", [start, end])
+  publiSub.publish(InterchageNotesP, [start, end])
   publiSub.publish(getDP, this)
 }
 
 function undoAction () {
-  publiSub.publish("undoActionPresenter")
+  publiSub.publish(undoActionP)
   publiSub.publish(getDP, this)
 }
 
@@ -74,122 +82,122 @@ function start () {
   function undoActionModelLoger() {
     model.undoAction()
   }
-  publiSub.subscribe("undoActionPresenter", undoActionModelLoger)
+  publiSub.subscribe(undoActionP, undoActionModelLoger)
   
   function interchageNotesModelLogger () {
-    let info = arguments[1]
+    let info=arguments[1]
     model.interchangeNotes(info[0], info[1])
   }
-  publiSub.subscribe("interchageNotesP", interchageNotesModelLogger)
+  publiSub.subscribe(InterchageNotesP, interchageNotesModelLogger)
 
   function filterNotesModelLogger () {
-    let filter = arguments[1]
+    let filter=arguments[1]
     model.filterNotes(filter)
   }
-  publiSub.subscribe("filterNotesPresenter", filterNotesModelLogger)
+  publiSub.subscribe(filterNotesP, filterNotesModelLogger)
 
   function getDatesModelLogger () {
-    let info = arguments[1]
-    const p = info[0]
-    const ids = info[1]
-    p.creationDate = model.getDate(ids, "creation")
-    p.lastMod = model.getDate(ids, "modification")
+    let info=arguments[1]
+    const p=info[0]
+    const ids=info[1]
+    p.creationDate=model.getDate(ids, "creation")
+    p.lastMod=model.getDate(ids, "modification")
   }
-  publiSub.subscribe("getDatesPresenter", getDatesModelLogger)
+  publiSub.subscribe(getDatesP, getDatesModelLogger)
 
   function deleteNoteModelL () {
-    let ids = arguments[1]
+    let ids=arguments[1]
     model.deleteNote(ids)
   }
-  publiSub.subscribe("deleteNotePresenter", deleteNoteModelL)
+  publiSub.subscribe(deleteNoteP, deleteNoteModelL)
 
   function editNoteModelL () {
-    let info = arguments[1]
+    let info=arguments[1]
     model.updateNote(info[0], info[1])
   }
-  publiSub.subscribe("editNotePresenter", editNoteModelL)
+  publiSub.subscribe(editNoteP, editNoteModelL)
 
   function saveNoteModelL () {
-    let note = arguments[1]
+    let note=arguments[1]
     model.saveNote(note)
   }
-  publiSub.subscribe("saveNotePresenter", saveNoteModelL)
+  publiSub.subscribe(saveNoteP, saveNoteModelL)
 
   function interchangeNotesLogger () {
-    let indexes = arguments[1]
+    let indexes=arguments[1]
     presenter.interchangeNotes(indexes[0], indexes[1])
-    const activeNotes = presenter.data
+    const activeNotes=presenter.data
     view.main(activeNotes)
   };
   publiSub.subscribe("interchangeNotes", interchangeNotesLogger)
 
   function textFilterLogger () {
-    let filter = arguments[1]
+    let filter=arguments[1]
     presenter.filterNotes(filter)
-    const activeNotes = presenter.data
+    const activeNotes=presenter.data
     view.main(activeNotes)
   }
   publiSub.subscribe("newText", textFilterLogger)
 
   function deleteNoteLogger () {
-    let ids = arguments[1]
+    let ids=arguments[1]
     presenter.deleteNote(ids)
-    const activeNotes = presenter.data
+    const activeNotes=presenter.data
     view.main(activeNotes)
   }
   publiSub.subscribe("deleteNote", deleteNoteLogger)
 
   function editNoteLogger () {
-    let info = arguments[1]
+    let info=arguments[1]
     presenter.editNote(info[0], info[1])
   }
   publiSub.subscribe("editNote", editNoteLogger)
 
   function saveNoteLogger () {
-    let note = arguments[1]
+    let note=arguments[1]
     presenter.saveNote(note)
-    const activeNotes = presenter.data
+    const activeNotes=presenter.data
     view.main(activeNotes)
   }
   publiSub.subscribe("saveNote", saveNoteLogger)
 
   function undoActionLogger () {
     presenter.undoAction()
-    const activeNotes = presenter.data
+    const activeNotes=presenter.data
     view.main(activeNotes)
   }
   publiSub.subscribe("undoAction", undoActionLogger)
 
   function startAppLogger () {
-    let activeNotes = arguments[1]
+    let activeNotes=arguments[1]
     view.start(activeNotes)
   }
   publiSub.subscribe("startApp", startAppLogger)
 
   function editClickedLogger () {
-    let ids = arguments[1]
-    const dates = presenter.dates(ids)
-    const activeNotes = presenter.data
-    const note = activeNotes[ids].note
+    let ids=arguments[1]
+    const dates=presenter.dates(ids)
+    const activeNotes=presenter.data
+    const note=activeNotes[ids].note
     view.edit(note, dates, ids)
   }
   publiSub.subscribe("editClicked", editClickedLogger)
 
   function saveEditClickedLogger () {
-    let info = arguments[1]
-    const ids = info[0]
-    const checker = info[1]
-    const data = presenter.data
-    const newNote = data[ids].note
+    let info=arguments[1]
+    const ids=info[0]
+    const checker=info[1]
+    const data=presenter.data
+    const newNote=data[ids].note
     view.saveEdition(newNote, ids, data, checker)
   }
   publiSub.subscribe("saveEditClicked", saveEditClickedLogger)
 
   function viewLogger () {
-    let ids = arguments[1]
-    const dates = presenter.dates(ids)
-    const activeNotes = presenter.data
-    const note = activeNotes[ids].note
+    let ids=arguments[1]
+    const dates=presenter.dates(ids)
+    const activeNotes=presenter.data
+    const note=activeNotes[ids].note
     view.view(ids, note, dates)
   }
   publiSub.subscribe("viewClicked", viewLogger)
